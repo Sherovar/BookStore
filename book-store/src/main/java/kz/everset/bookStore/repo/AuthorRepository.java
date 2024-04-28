1bebedf0a2df07ec8d65f34ac96ff54d8c92c48d
Sherovar <64183894+Sherovar@users.noreply.github.com>
2024-04-28 13:55:09 +0500

    Delete book-store directory

--- book-store/src/main/java/kz/everset/bookStore/repo/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package kz.everset.bookStore.repo;
-
-import kz.everset.bookStore.domain.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
--- book-store/src/main/java/kz/everset/bookStore/repo/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package kz.everset.bookStore.repo;
-
-import kz.everset.bookStore.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
